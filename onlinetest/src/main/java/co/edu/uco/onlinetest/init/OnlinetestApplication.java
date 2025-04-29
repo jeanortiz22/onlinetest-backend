161deb44d2dfa6c4933bdffa204026072d3f3cd6
Jean Paul Ortiz <jean.ortiz1424@uco.net.co>
2025-04-29 00:30:13 -0500

    Carga línea base proyecto de clase
    Carga del proyecto como linea base con el scaffolding de una aplicacion
    web spring boot.

---
+++ onlinetest/src/main/java/co/edu/uco/onlinetest/init/OnlinetestApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.onlinetest.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinetestApplication.class, args);
+	}
+
+}
